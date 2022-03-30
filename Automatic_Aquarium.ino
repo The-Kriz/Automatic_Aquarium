12dddc035d7d89bfcbd149e19d5c3f2ecf054aac
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2022-03-30 12:45:35 +0530

    Get Temperature function

--- Automatic_Aquarium.ino
+++ Automatic_Aquarium.ino
@@ -1,9 +1,34 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 5
+float Celcius = 0;
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+
+
+
+void setup() 
+{
+   
+  Serial.begin(9600);
+  sensors.begin();
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  
+  getTemperature();
 
+}
+
+void getTemperature()
+{
+  sensors.requestTemperatures(); 
+  Celcius=sensors.getTempCByIndex(0);
+  Serial.print(" C  ");
+  Serial.print(Celcius);
+  return Celcius;
 }
